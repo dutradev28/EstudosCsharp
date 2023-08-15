b05c5bd893018a910bf31bcee04fd08dda2670f1
Luis Carlos Dutra Jr <luiscarlosdutrajunior23@gmail.com>
2023-08-15 19:24:58 -0300

    exercicios curso csharp

---
+++ uri1016/uri1016/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace uri1016 { 
+    class Program { 
+        static void Main(string[] args) {
+
+            int X;
+            double Y, kml;
+
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            kml = (double)X / Y;
+
+            Console.WriteLine(kml.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        
+        
+        
+        
+        }
+    }
+}
