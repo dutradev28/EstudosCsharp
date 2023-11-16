807fa2d31df4874cbcb92ef0f6900ad55a2bc296
Luis Carlos Dutra Jr <luiscarlosdutrajunior23@gmail.com>
2023-11-16 19:48:31 -0300

    exercicio preposto 1

---
+++ curso-poo/exproposto1/exproposto1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int soma = a + b;
+
+            Console.WriteLine("Soma = " + soma);
+
+        }
+    }
+}
