3f0bd583fdd50952b83d505a03034e58ef0251b0
Luis Carlos Dutra Jr <luiscarlosdutrajunior23@gmail.com>
2023-11-16 19:02:33 -0300

    exercício de fixação

---
+++ curso-poo/exFixacao/exFixacao/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Entre com seu nome completo:");
+            string fullname = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int bedrooms = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre o preço do produto:");
+            double price = double.Parse(Console.ReadLine());
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] vet = Console.ReadLine().Split(' ');
+            string lastname = vet[0];
+            int age = int.Parse(vet[1]);
+            double height = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(fullname);
+            Console.WriteLine(bedrooms);
+            Console.WriteLine(price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(lastname);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
