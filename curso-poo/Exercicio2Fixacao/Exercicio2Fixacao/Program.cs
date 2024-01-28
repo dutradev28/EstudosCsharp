c927af229cb13876002240206483395c61d7076f
Luis Carlos Dutra Jr <luiscarlosdutrajunior23@gmail.com>
2024-01-28 14:23:41 -0300

    Estudos POO

---
+++ curso-poo/Exercicio2Fixacao/Exercicio2Fixacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Funcionario func = new Funcionario();
+
+            Console.WriteLine("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.WriteLine("Salário bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionário " + func);
+
+            Console.WriteLine();
+            Console.WriteLine("Deseja aumentar o salário em qual porcentagem? ");
+            double porcent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+        }
+    }
+}
--- curso-poo/SegundoProblemaclasse/SegundoProblemaclasse/Program.cs
+++ curso-poo/SegundoProblemaclasse/SegundoProblemaclasse/Program.cs
@@ -19,6 +19,23 @@ namespace Course {
 
             Console.WriteLine();
             Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionados no estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
         }
     }
 }
