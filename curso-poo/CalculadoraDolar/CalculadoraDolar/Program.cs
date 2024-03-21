5576835169e1f2c1a0df42590285b5501f208e9e
Luis Carlos Dutra Jr <luiscarlosdutrajunior23@gmail.com>
2024-03-21 15:33:21 -0300

    Continuação estudos
    Estudos POO C#

---
+++ curso-poo/CalculadoraDolar/CalculadoraDolar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculadoraDolar {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoeda.DolarParaReal(quantia, cotacao);
+
+            Console.WriteLine("Valor a ser pagao em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+
+
+
+}
