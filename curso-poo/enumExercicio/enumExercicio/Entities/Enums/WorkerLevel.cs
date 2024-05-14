7c382ae225bef65591a22429d9928cf27f34a880
Dutra <luiscarlosdutrajunior23@gmail.com>
2024-05-14 17:02:45 -0300

    Exercicio resolvido de enumeração + composição

---
+++ curso-poo/enumExercicio/enumExercicio/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿using enumExercicio.Entities.Enums;
+
+namespace enumExercicio.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
