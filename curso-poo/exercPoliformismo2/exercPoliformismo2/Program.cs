6cfa01be8f77fcd65e58c62414fd9c7a88170ca7
Dutra <luiscarlosdutrajunior23@gmail.com>
2024-05-16 17:54:03 -0300

    Resolvido exercicio Poliformismo 2

---
+++ curso-poo/exercPoliformismo2/exercPoliformismo2/Program.cs
@@ -0,0 +1,53 @@
+﻿using exercPoliformismo2.Entities;
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace exercPoliformismo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            List<Product> list = new List<Product>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data: ");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char type = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                if (type == 'c')
+                {
+                    list.Add(new Product(name, price));
+                }
+                else if (type == 'u')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else
+                {
+                    Console.Write("Customs Fee: ");
+                    double customsfee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportedProduct(name, price, customsfee));
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("PRICE TAGS: ");
+            foreach (Product prod in list)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+
+        }
+    }
+}
